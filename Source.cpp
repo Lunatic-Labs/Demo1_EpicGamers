dc5c389ae6f60c6bc05c3f4834c58724ea3de1f4
alexanderam19 <91917921+alexanderam19@users.noreply.github.com>
2022-09-08 21:14:07 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+
+int main() {
+
+	std::cout << "Hello\nEPIC\nGAMERS\n";
+
+	system("pause");
+
+	return 0;
+}
