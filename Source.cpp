5a779f6e3461be27bc1ca07913cea6fd953ca718
alexanderam19 <91917921+alexanderam19@users.noreply.github.com>
2022-09-29 01:32:59 -0500

    Dog Animation added

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,43 @@
 #include <SFML/Graphics.hpp>
+#include "Animation.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML works!");
+    sf::RectangleShape player(sf::Vector2f(250.0f, 220.0f));
+    player.setPosition(100.0f, 250.0f);
+    sf::Texture dogPlayer;
+    dogPlayer.loadFromFile("Textures/googleDog.jpg");
+    player.setTexture(&dogPlayer);
+
+    Animation animation(&dogPlayer, sf::Vector2u(6, 2), 0.3f);
+
+    float deltaTime = 0.0f;
+    sf::Clock clock;
+
+
+    //Vector for Sprit
+    /*sf::Vector2u textureSize = dogPlayer.getSize();
+    textureSize.x /= 6;
+    textureSize.y /= 2;
+
+    player.setTextureRect(sf::IntRect(textureSize.x*5, textureSize.y*1, textureSize.x, textureSize.y));*/
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        deltaTime = clock.restart().asSeconds();
+        sf::Event evnt;
+        while (window.pollEvent(evnt))
         {
-            if (event.type == sf::Event::Closed)
+            if (evnt.type == sf::Event::Closed)
                 window.close();
         }
 
+        animation.update(0, deltaTime);
+        player.setTextureRect(animation.uvRect);
+
         window.clear();
-        window.draw(shape);
+        window.draw(player);
         window.display();
     }
 
