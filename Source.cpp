87c4da3b6621b8cc0c0ae6b2b5c0914d2f506223
Brett Boyd <79927828+bboyd01@users.noreply.github.com>
2022-10-04 12:40:53 -0500

    Added comments and organized Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,16 +4,22 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML works!");
+
+    // load player
     sf::Texture dogPlayer;
     dogPlayer.loadFromFile("Textures/walkJump.png");
     Player player(&dogPlayer, sf::Vector2u(6, 2), 0.09f, 100.0f);
 
+    // create time
     float deltaTime = 0.0f;
     sf::Clock clock;
 
+    // core game loop
     while (window.isOpen())
     {
         deltaTime = clock.restart().asSeconds();
+
+        // close window if exited
         sf::Event evnt;
         while (window.pollEvent(evnt))
         {
@@ -21,8 +27,10 @@ int main()
                 window.close();
         }
 
+        // process player
         player.Update(deltaTime);
 
+        // clear the window and draw the next frame
         window.clear();
         player.Draw(window);
         window.display();
