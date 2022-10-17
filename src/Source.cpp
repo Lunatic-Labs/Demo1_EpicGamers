fc77e28bf0c0826fbc877de98aeb16909ba2675c
Brett Boyd <bboydmusic01@gmail.com>
2022-10-17 14:53:02 -0500

    improved formatting a bit more on Source

--- src/Source.cpp
+++ src/Source.cpp
@@ -13,9 +13,9 @@ void resizeView(const sf::RenderWindow& window, sf::View& view)
 }
 
 
-//main function 
 int main()
 {
+    // create and setup window
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Dog Dash!");
     sf::Image icon;
     icon.loadFromFile("Textures/LUlogo.jpg");
@@ -23,13 +23,14 @@ int main()
     sf::View view(sf::Vector2f(100.0f, 500.0f), sf::Vector2f(VIEW_WIDTH,VIEW_HEIGHT));
 
     // init important variables
-    float gameSpeed = 1.75;                     // lower gameSpeed = faster movement.
+    float gameSpeed = 1.75;                     // lower gameSpeed = faster movement
     float gameSpeedLimit = 1.25;                // gameSpeed variable cannot decrease below this limit
     float gameSpeedModifier = 0.05;             // change the speed by *this* much
     int incrementWaitTime = 14;                 // increases speed every x seconds
     int loopCounter = 1;                        // tracks the number of speed increment loops
+    bool isJumping = false;                     // prevents multiple simultaneous jump inputs
     float deltaTime = 0.0f, totalTime = 0.0f;
-    
+    sf::Clock clock, jumpTimer;
   
     // load player and ground textures and init objects
     sf::Texture dogPlayer, groundScroll;
@@ -38,29 +39,25 @@ int main()
     Player player(&dogPlayer, sf::Vector2u(10, 2), 0.09f, 100.0f);
     Ground ground(&groundScroll, sf::Vector2u(4, 1), 0.09f, 100.0f);
 
-    sf::Clock clock, jumpTimer;
-    bool isJumping = false;                     // prevents multiple simultaneous jump inputs
-
-
     // core game loop
-    while (window.isOpen())     
-    {
-        totalTime += clock.getElapsedTime().asSeconds();    //Total game time
-        deltaTime = clock.restart().asSeconds();            //Time each loop, < 1.0s
+    while (window.isOpen()) {
+        // manage time since last frame
+        totalTime += clock.getElapsedTime().asSeconds();
+        deltaTime = clock.restart().asSeconds();
 
         // handle events
         sf::Event evnt;
         while (window.pollEvent(evnt)) {
             //Check the type of the event
             switch (evnt.type) {
-                // window events
+                // process window events
                 case sf::Event::Closed:
                     window.close();
                     break;
                 case sf::Event::Resized:
                     resizeView(window, view);
                     break;
-                // keyboard input
+                // process key presses
                 case sf::Event::KeyPressed: 
                     // spacebar -> jump
                     if (evnt.key.code == 57 && isJumping == false) {   
@@ -68,6 +65,7 @@ int main()
                         player.Update(deltaTime, isJumping, gameSpeed-0.25);        // !! question- what is the 0.25 for? Let's use variables (with documentation comments) for these types of things.
                     }
                     break;
+                // process key releases
                 case sf::Event::KeyReleased:
                     if (evnt.key.code == 57) {
                         isJumping = false;
@@ -88,8 +86,7 @@ int main()
         ground.Draw(window);
         window.display();
 
-        // increase speed, with an upper limit
-        // lower speed == faster scroll with this animation functionality
+        // increase speed if needed, with an upper limit. Again, lower gameSpeed = faster movement
         if (totalTime > (incrementWaitTime*loopCounter) && gameSpeed > gameSpeedLimit) {
             loopCounter++;
             gameSpeed -= gameSpeedModifier;
