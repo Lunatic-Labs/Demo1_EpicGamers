e561160145185d5decd0da80ca8029f56a4896fe
Brett Boyd <79927828+bboyd01@users.noreply.github.com>
2022-10-18 12:34:10 -0500

    improved formatting in header files

--- include/Ground.h
+++ include/Ground.h
@@ -2,18 +2,17 @@
 #include <SFML/Graphics.hpp>
 #include "Animation.h"
 
-//Setting up similarly to the Player class, but only animation functionality. 
-class Ground
-{
-public:
-	Ground(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
-	~Ground();
+// set up similarly to the Player class, but only animation functionality. 
+class Ground {
+	public:
+		Ground(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
+		~Ground();
 
-	void Update(float deltaTime, float speedMultiplier);
-	void Draw(sf::RenderWindow& window);
-	sf::Vector2f getPosition() { return sidewalk.getPosition(); }
-private:
-	sf::RectangleShape sidewalk;
-	Animation animation;
-	float speed;
+		void Update(float deltaTime, float speedMultiplier);
+		void Draw(sf::RenderWindow& window);
+		sf::Vector2f getPosition() { return sidewalk.getPosition(); }
+	private:
+		sf::RectangleShape sidewalk;
+		Animation animation;
+		float speed;
 };
--- include/Player.h
+++ include/Player.h
@@ -2,23 +2,22 @@
 #include <SFML/Graphics.hpp>
 #include "Animation.h"
 
-class Player
-{
-public:
-	Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
-	~Player();
+class Player {
+	public:
+		Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
+		~Player();
 
-	void Update(float deltaTime, bool jumpInput, float speedMultiplier);
-	void Draw(sf::RenderWindow& window);
-	sf::Vector2f getPosition() { return body.getPosition(); }
+		void Update(float deltaTime, bool jumpInput, float speedMultiplier);
+		void Draw(sf::RenderWindow& window);
+		sf::Vector2f getPosition() { return body.getPosition(); }
 
-private:
-	sf::RectangleShape body;
-	Animation animation;
-	sf::Vector2f movement{ 0.0f, 0.0f };
-	sf::Clock jumpTimer;
-	unsigned int row;
-	float speed;
-	bool faceRight, startJump{ false };
+	private:
+		sf::RectangleShape body;
+		Animation animation;
+		sf::Vector2f movement{ 0.0f, 0.0f };
+		sf::Clock jumpTimer;
+		unsigned int row;
+		float speed;
+		bool faceRight, startJump{ false };
 };
 
