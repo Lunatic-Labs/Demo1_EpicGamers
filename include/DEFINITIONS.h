29a1c8525ca79e6935bc0d206662f2bc4b781abb
Cole Carter <112132207+cdcarter21@users.noreply.github.com>
2022-11-02 22:14:14 -0500

    Updating game title file path

--- include/DEFINITIONS.h
+++ include/DEFINITIONS.h
@@ -10,7 +10,7 @@
 #define GAME_BACKGROUND_FILEPATH "Resources/Textures/LUlogo.jpg"
 #define GAME_OVER_BACKGROUND_FILEPATH "Resources/Textures/LUlogo.jpg"
 
-#define GAME_TITLE_FILEPATH "Resources/Textures/dogWIP2.jpg"
+#define GAME_TITLE_FILEPATH "Resources/Textures/title.png"
 #define PLAY_BUTTON_FILEPATH "Resources/Textures/SFMLPlayButton.png"
 
 #define PLAYER_FRAME_1_FILEPATH "Resources/Player/dogRunner Frame 1.png"		//Tutorial defined filepaths to each frame of animation separately. Compare methods.
