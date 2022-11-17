2f13730966c88ad03c0e6770b121d90898ae94dd
Cole <coltondcarter21@gmail.com>
2022-11-16 20:44:41 -0600

    Display Score (#29)

--- src/GameOverState.cpp
+++ src/GameOverState.cpp
@@ -7,8 +7,8 @@
 #include "../include/GameState.h"
 
 namespace EpicGamers
-{
-	GameOverState::GameOverState(GameDataRef data) : data(data)
+{				//add int score parameter here  Cole #29 Video
+	GameOverState::GameOverState(GameDataRef data) : data(data) //add ,score(score);	Cole #29 Video
 	{
 
 	}
@@ -27,6 +27,20 @@ namespace EpicGamers
 		//gameOverContainer.setPosition((data->window.getSize().x / 2) - (gameOverContainer.getGlobalBounds().width / 2),(data->window.getSize().y / 2) - (gameOverContainer.getGlobalBounds().height / 2));	Cole #28 Video
 		//gameOverTitle.setPosition((data->window.getSize().x / 2) - (gameOverTitle.getGlobalBounds().width / 2), gameOverContainer.getPosition().y - (gameOverTitle.getGlobalBounds().height * 1.2));			Cole #28 Video
 		//retryButton.setPosition((data->window.getSize().x / 2) - (retryButton.getGlobalBounds().width / 2), gameOverContainer.getPosition().y + gameOverContainer.getGlobalBounds().height + (retryButton.getGlobalBounds().height * 0.2));	Cole #28 Video
+	
+		//scoreText.setFont(data->assets.GetFont("Dog Font"));		Cole #29 Video
+		//scoreText.setString(std::to_string(score));				Cole #29 Video
+		//scoreText.setCharacterSize(56);							Cole #29 Video
+		//scoreText.setFillColor(sf::Color::White);
+		//scoreText.setOrigin(scoreText.getGlobalBounds().width / 2, scoreText.getGlobalBounds().height / 2);		Cole #29 Video
+		//scoreText.setPosition(data->window.getSize().x / 10 * 7.25, data->window.getSize().y / 2.15);				Cole #29 Video
+	
+		//highScoreText.setFont(data->assets.GetFont("Dog Font"));		Cole #29 Video
+		//highScoreText.setString(std::to_string(highScore));				Cole #29 Video
+		//highScoreText.setCharacterSize(56);							Cole #29 Video
+		//highScoreText.setFillColor(sf::Color::White);
+		//highScoreText.setOrigin(highScoreText.getGlobalBounds().width / 2, highScoreText.getGlobalBounds().height / 2);		Cole #29 Video
+		//highScoreText.setPosition(data->window.getSize().x / 10 * 7.25, data->window.getSize().y / 1.78);				Cole #29 Video
 	}
 
 	void GameOverState::HandleInput()
@@ -60,6 +74,8 @@ namespace EpicGamers
 		//data->window.draw(gameOverTitle);		Cole #28 Video
 		//data->window.draw(gameOverContainer);	Cole #28 Video
 		//data->window.draw(RetryButton);		Cole #28 Video
+		//data->window.draw(scoreText);			Cole #29 Video
+		//data->window.draw(highScoreText);		Cole #29 Video
 		data->window.display();
 	}
 }
